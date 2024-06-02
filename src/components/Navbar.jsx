008a65744538452101a6c16499a8b19ce6b32da3
Prasenjit Das <prasenjitpriyan@gmail.com>
2024-06-02 22:46:58 +0530

    Spinner modified

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ const Navbar = () => {
     <nav className="flex justify-center bg-white w-full border-b md:border-0 md:static">
       <div className="items-center px-4 max-w-screen-xl mx-auto md:flex md:px-8">
         <div className="flex items-center justify-between py-3 md:py-5 md:block">
-          <a className="animate-logo text-4xl font-semibold" to="/">
+          <a className="text-4xl font-semibold" href="/">
             StaffHub
           </a>
         </div>
